34f39cd63b182eadfbb4cb7151388566b14368c2
Idrees Hassan <idrees@idreesinc.com>
2022-01-08 15:10:13 -0500

    Give Porygon some moves

--- src/application.js
+++ src/application.js
@@ -1,4 +1,5 @@
 import "./stylesheet.css";
+import "./porygon-sprite.png";
 import porygonMeshPath from "./porygon/porygon.glb";
 import backgroundPath from "./gradient.jpg";
 import * as THREE from "three";
@@ -16,13 +17,19 @@ let container = document.getElementById("viewer");
 let controls;
 let loader = new GLTFLoader();
 let textureLoader = new THREE.TextureLoader();
+let porygon;
+let rightFoot;
+let tail;
+let head;
+let leftFoot;
+let endWaist;
 
 function init() {
     stats = Stats();
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     
     camera = new THREE.PerspectiveCamera(50, getWidth() / getHeight(), 1, 75);
-    camera.position.set(-3, 2.8, 4);
+    camera.position.set(-3, 2.9, 3.5);
     
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -33,12 +40,14 @@ function init() {
     container.appendChild(renderer.domElement);
     
     controls = new OrbitControls(camera, renderer.domElement);
-    camera.lookAt(new THREE.Vector3(0, 0.2, 0));
+    const TARGET_Y = 0.3;
+    camera.lookAt(new THREE.Vector3(0, TARGET_Y, 0));
+    controls.target.set(0, TARGET_Y, 0);
 
     const light = new THREE.AmbientLight( 0xffffff, 2 ); // soft white light
     scene.add( light );
 
-    const light2 = new THREE.PointLight( 0xff00ae, 5, 500 );
+    const light2 = new THREE.PointLight( 0xff00ae, 5, 100 );
     light2.position.set( 10, -50, 50 );
     scene.add( light2 );
 
@@ -78,12 +87,28 @@ function loadPorygon() {
         function ( gltf ) {
             scene.add( gltf.scene );
 
+            porygon = gltf.scene;
+
             gltf.scene.traverse(function ( child ) {
                 if (child instanceof THREE.Mesh) {
                     console.log(child.name);
                 }
             });
 
+            // console.log(gltf);
+
+            let armature = gltf.scene.children[0];
+            let pm0137_00 = armature.children[0];
+            let origin = pm0137_00.children[0];
+            let waist = origin.children[0];
+
+            console.log(waist.children);
+
+            rightFoot = waist.children[0];
+            tail = waist.children[1];
+            head = waist.children[2];
+            leftFoot = waist.children[3];
+            endWaist = waist.children[4];
 
             gltf.animations; // Array<THREE.AnimationClip>
             gltf.scene; // THREE.Group
@@ -104,9 +129,32 @@ function loadPorygon() {
 
 function animate() {
     stats.begin();
+    animatePorygon();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     stats.end();
+}
+
+function animatePorygon() {
+    let milliseconds = Date.now();
+    if (tail) {
+        const RANGE = Math.PI * 0.1;
+        const ORIGIN = 0.855842168538818
+        tail.rotation.y  = Math.sin(milliseconds * 0.015) * RANGE + ORIGIN;
+    }
+    if (leftFoot) {
+        leftFoot.rotation.z = Math.sin(milliseconds * 0.01) * Math.PI * 0.1;
+    }
+    if (rightFoot) {
+        rightFoot.rotation.z = Math.sin(milliseconds * 0.01) * Math.PI * 0.1 + Math.PI;
+    }
+    if (head) {
+        const ORIGIN = -0.3530296061149312;
+        head.rotation.y  = Math.sin(milliseconds * 0.01) * Math.PI * 0.015 + ORIGIN;
+    }
+    if (porygon) {
+        porygon.position.set(0, Math.sin(milliseconds * 0.0045) * Math.PI * 0.03, 0);
+    }
 }
 
 function getWidth() {
